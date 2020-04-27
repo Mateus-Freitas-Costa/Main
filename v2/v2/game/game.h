9040a2a5a082b0f41a983b66d8cdaf075ef56e22
Mateuzz <38082919+vactus@users.noreply.github.com>
2020-04-27 16:54:15 -0300

    Add files via upload

---
+++ v2/v2/game/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "../main/extra.h"
+
+Winner two_players_game (const Info info);
+Winner bot_game         (const Info info);
+// not working outside
+void clear_screen       (void);
+
+#endif /* GAME_H */
---
+++ v2/v2/main/extra.h
@@ -0,0 +1,13 @@
+#ifndef EXTRA_H
+#define EXTRA_H
+
+typedef enum {PLAYER_ONE, PLAYER_TWO, BOT} Winner;
+
+typedef struct {
+    int  boats;
+    int  mode;
+    int  difficulty;
+} Info;
+
+
+#endif /* EXTRA_H */
