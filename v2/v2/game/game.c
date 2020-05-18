6a71449b00fdf2b4a0f0abb8d62715270403f320
Mateuzz <38082919+vactus@users.noreply.github.com>
2020-05-17 23:16:42 -0300

    Update game.c

--- v2/v2/game/game.c
+++ v2/v2/game/game.c
@@ -6,171 +6,159 @@
 #include "../battle/battle.h"
 #include "../bot/bot.h"
 
+void clear_screen(void);
+char *fgets_(char *str, size_t size, FILE *stream);
+static void display_HUD(const Map *map, int current_height);
+static void display(const Map map[]);
+static bool destroyed_boat(Map *map, Boat *boat, Point attack);
+static bool check_quit(Point input);
+static Point input_attack_player(char *self_name);
+static Point attack_player(Instance *instance, Map *map_game, Boat *boats);
+static void create_player(Instance *new_instance, Tag tag, char *idenfifier, Player enemy, Winner name);
+static Point bot_response(Instance *instance, Map *map_game, Boat *boats);
+static bool round(Instance *instance, Map *map, size_t boats_count, Boat boats[MAX_PLAYERS][boats_count]);
+static void create_instances(Instance *instances, Info info);
+static void destroy_instances(Instance *instances, int boats_count, Boat boats[MAX_PLAYERS][boats_count]);
+Winner play_game(Info info);
 
-static bool destroyed_boat   (Map *map, Boat *p, Point attack);
-static void display          (const Map map[]);
-static void display_HUD      (const Map map[], int current_height);
-static bool check_quit       (Point input);
-static bool response         (Point attack, Map map[], Boat *p[],  Player enemy);
-static void bot_response     (Map map[], Point attack, Boat *p[]);
-static void destroy_boats    (Boat *p1[], Boat *p2[], size_t boats);
-
-
-Winner bot_game(Info info)
+Winner play_game(Info info)
 {
-    // Those pointers are the most stupid thing you ever have seen
-    Boat *p1[info.boats];
-    Boat *p2[info.boats];
-    Map   map[MAX_PLAYERS];
-    AI    bot;
-    create_map(p1, p2, map, info);
-    create_bot(&bot, info.difficulty);
-    display(map);
-    Point attack;
-    for (;;) {
-        printf("Player one time, enter a coordinate (x-y): ");
-        scanf("%d -%d", &attack.x, &attack.y);
-        if (check_quit(attack)) {
-            puts("You quit the game");
-            destroy_boats(p1, p2, info.boats);
-            exit(EXIT_SUCCESS);
-        }
-        if (!response(attack, map, p2, OPPONENT)) 
-            continue;
-        if (map[OPPONENT].remaining_boats == 0)
-            return PLAYER_ONE;
-
-        puts("BOT time");
-        sleep(1);
-        attack = bot_time(&bot, &map[PLAYER1], p1);
-        bot_response(map, attack, p1);
-        if (map[PLAYER1].remaining_boats == 0)
-            return BOT;
-    }
-    
-
-    destroy_boats(p1, p2, info.boats);
-}
-
-
-Winner two_players_game(Info info)
-{
-    Boat *p1[info.boats];
-    Boat *p2[info.boats];
+    Boat boats[MAX_PLAYERS][info.boats];
+    Instance instances[MAX_PLAYERS];
     Map map[MAX_PLAYERS];
+    Player i = PLAYER1;
 
-    create_map(p1, p2, map, info);
+    create_instances(instances, info);
+    create_map(info, map, boats);
     display(map);
-    Point attack;
+
     for (;;) {
-        for (;;) {
-            printf("Player one time, enter a coordinate (x-y): ");
-            scanf("%d -%d", &attack.x, &attack.y);
-            if (check_quit(attack)) {
-                puts("You quit the game");
-                destroy_boats(p1, p2, info.boats);
-                exit(EXIT_SUCCESS);
-            }
-            if (!response(attack, map, p2, OPPONENT)) 
-                continue;
-            if (map[OPPONENT].remaining_boats == 0)
-                return PLAYER_ONE;
-            break;
+        bool victory = round(&instances[i], map, info.boats, boats);
+        if (victory) {
+            destroy_instances(instances, info.boats, boats);
+            return instances[i].name;
         }
-        for (;;) {
-            printf("Player two time, enter a coordinate (x-y): ");
-            scanf("%d -%d", &attack.x, &attack.y);
-            if (check_quit(attack)) {
-                puts("You quit the game");
-                destroy_boats(p1, p2, info.boats);
-                exit(EXIT_SUCCESS);
-            }
-            if (!response(attack, map, p1, PLAYER1)) 
-                continue;
-            if (map[PLAYER1].remaining_boats == 0)
-                return PLAYER_TWO;
-            break;
+        i = i == PLAYER1 ? OPPONENT : PLAYER1;
+    }
+
+}
+
+static void destroy_instances(Instance *instances, int boats_count, Boat boats[MAX_PLAYERS][boats_count])
+{
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
+        if (instances[i].tag == CPU)
+            free(instances[i].self);
+        for (int j = 0; j < boats_count; ++j)
+            free(boats[i][j].p);
+    }
+}
+
+static void create_instances(Instance *instances, Info info)
+{
+    create_player(&instances[PLAYER1], HUMAN, "Player one", OPPONENT, PLAYER_ONE);
+
+    switch (info.mode) {
+    case 1:
+        instances[OPPONENT].tag = CPU;
+        instances[OPPONENT].name = BOT;
+        instances[OPPONENT].attack_func = bot_response;
+        instances[OPPONENT].self = create_bot(info.difficulty);
+        instances[OPPONENT].enemy = PLAYER1;
+        break;
+    case 2:
+        create_player(&instances[OPPONENT], HUMAN, "Player Two", PLAYER1, PLAYER_TWO);
+        break;
+    }
+}
+
+static bool round(Instance *instance, Map *map, size_t boats_count, Boat boats[MAX_PLAYERS][boats_count])
+{
+    Point attack = instance->attack_func(instance, map, boats[instance->enemy]);
+    char *msgs_attacks[MAX_PLAYERS][3] = {{"You attacked ", "You destroyed ", "You Missed"},
+                                          {"The Bot attacked your ", "The bot destroyed your ", "The bot Missed"}};
+    char msg_attack[25];
+    Boat *attacked = get_boat(boats[instance->enemy], attack, boats_count);
+
+    if (attacked == NULL) {
+        sprintf(msg_attack, "%s", msgs_attacks[instance->tag][2]);
+        map[instance->enemy].sea[attack.y][attack.x] = MISSED;
+    } else {
+        int index_msg;
+
+        if (destroyed_boat(&map[instance->enemy], attacked, attack)) {
+            index_msg = 1;
+            update_HUD(&map[instance->enemy].HUD, attacked->type, -1);
+        } else
+            index_msg = 0;
+        sprintf(msg_attack, "%s%s", msgs_attacks[instance->tag][index_msg], attacked->name);
+    }
+
+    display(map);
+    printf("%s\n", msg_attack);
+
+    return map[instance->enemy].remaining_boats == 0;
+}
+
+static Point bot_response(Instance *instance, Map *map, Boat *boats)
+{
+    AI *bot = instance->self;
+
+    puts("Bot time\n");
+    sleep(1);
+    return bot_time(bot, map, boats);
+}
+
+static void create_player(Instance *new_instance, Tag tag, char *idenfifier, Player enemy, Winner name)
+{
+    new_instance->enemy = enemy;
+    new_instance->self = idenfifier;
+    new_instance->name = name;
+    new_instance->attack_func = attack_player;
+    new_instance->tag = HUMAN;
+}
+
+static Point attack_player(Instance *instance, Map *map_game, Boat *boats)
+{
+    char *msg_invalid[2] = {"Outside of range", "You cannot attack the same point again"};
+    Map *map = map_game;
+    Point attack;
+    Validity validity;
+
+    do {
+        attack = input_attack_player(instance->self);
+        validity = check_valid(map[instance->enemy].sea, attack);
+        if (validity != VALID) {
+            display(map);
+            printf("%s\n", msg_invalid[validity]);
         }
-    }
-    destroy_boats(p1, p2, info.boats);
+    } while (validity != VALID);
+    
+    return attack;
 }
 
-static bool response(Point attack, Map map[], Boat *p[], Player enemy)
+static Point input_attack_player(char *self_name)
 {
-    Validity result;
-    if ((result = check_valid(attack, map[enemy].sea)) == OUTRANGE) {
-        display(map);
-        puts("Outside of Range");
-        return false;
-    } 
-    if (result == REPEATED) {
-        display(map);
-        puts("You cannot attack the same point again");
-        return false;
-    }
+    char ipt_str[10];
+    Point input;
 
-    Boat *attacked = get_boat(p, attack, map->info.boats);
-    if (attacked == NULL) {
-        map[enemy].sea[attack.y][attack.x] = MISSED;
-        display(map);
-        puts("You missed");
-    } else if (destroyed_boat(&map[enemy], attacked, attack)) {
-        update_HUD(&map[enemy].HUD, attacked->type, -1);
-        display(map);
-        printf("You destroyed %s\n", attacked->name);
-    } else {
-        display(map);
-        printf("You attacked %s\n", attacked->name);
-    }
-    return true;
+    printf("%s time, enter a coordinate: ", self_name);
+    fgets_(ipt_str, 9, stdin);
+    sscanf(ipt_str, "%d -%d", &input.x, &input.y);
+
+    return input;
 }
 
-static void bot_response(Map map[], Point attack, Boat *p[])
+static bool destroyed_boat(Map *map, Boat *boat, Point attack)
 {
-    Boat *attacked = get_boat(p, attack, map[PLAYER1].info.boats);
-    if (attacked == NULL) {
-        map[PLAYER1].sea[attack.y][attack.x] = MISSED;
-        display(map);
-        puts("The BOT missed");
-    } else if (destroyed_boat(&map[PLAYER1], attacked, attack)) {
-        update_HUD(&map[PLAYER1].HUD, attacked->type, -1);
-        display(map);
-        printf("The BOT destroyed your %s\n", attacked->name);
-    } else {
-        display(map);
-        printf("The BOT attacked your %s\n", attacked->name);
-    }
-}
-
-static bool destroyed_boat(Map *map, Boat *p, Point attack)
-{
-    --p->lifes;
     map->sea[attack.y][attack.x] = DESTROYED;
-    if (p->lifes == 0) {
+    if (--boat->lifes == 0) {
         --map->remaining_boats;
-        for (int i = 0; i < p->size; ++i)   
-            map->sea[p->coordinates[i].p.y]
-                    [p->coordinates[i].p.x] = SUNK;
+        for (int j = 0; j < boat->size; ++j)
+            map->sea[boat->p[j].y][boat->p[j].x] = SUNK;
         return true;
-    } 
-    return false;
+    } else
+        return false;
 }
-
-static void destroy_boats(Boat *p1[], Boat *p2[], size_t boats)
-{
-    for (int i = 0; i < boats; ++i) {
-        if (p1[i] != NULL) {
-            free(p1[i]->coordinates);
-            free(p1[i]);
-        }
-        if (p2[i] != NULL) {
-            free(p2[i]->coordinates);
-            free(p2[i]);
-        }
-    }
-}
-
-
 
 static bool check_quit(Point input)
 {
@@ -183,7 +171,7 @@ static void display(const Map map[])
     puts("--------------------------------------------------------------------------");
     puts("                                Battleship Game                           ");
     puts("--------------------------------------------------------------------------");
-    
+
     for (int i = 0; i < HEIGHT; ++i) {
         for (int j = 0; j < MAX_PLAYERS; ++j) {
             if (i == 0 && j == PLAYER1) {
@@ -203,19 +191,19 @@ static void display(const Map map[])
                 if (k == 0)
                     printf("%-2d-|", i);
                 switch (map[j].sea[i][k]) {
-                case INACTIVE:
-                case ACTIVE:
-                    printf("   ");
-                    break;
-                case DESTROYED:
-                    printf(" X ");
-                    break;
-                case MISSED:
-                    printf(" * ");
-                    break;
-                case SUNK:
-                    printf(" # ");
-                    break;
+                    case INACTIVE:
+                    case ACTIVE:
+                        printf("   ");
+                        break;
+                    case DESTROYED:
+                        printf(" X ");
+                        break;
+                    case MISSED:
+                        printf(" * ");
+                        break;
+                    case SUNK:
+                        printf(" # ");
+                        break;
                 }
             }
             printf("       ");
@@ -243,25 +231,40 @@ static void display_HUD(const Map *map, int current_height)
 
     printf("                              ");
     switch (current_height % 5) {
-    case 0:
-        printf("Destroyers: %u  |  Cruisers: %u", map[current_player].HUD.destroyers, 
-                map[current_player].HUD.cruisers);
-        break;
-    case 1:
-        printf("Submarines: %u  |  Battleships: %u ", map[current_player].HUD.submarines,
-                map[current_player].HUD.battleships);
-        break;
-    case 2:
-        printf("Carriers: %u    |", map[current_player].HUD.carriers);
-        break;
+        case 0:
+            printf("Destroyers: %u  |  Cruisers: %u", map[current_player].HUD.destroyers, 
+                    map[current_player].HUD.cruisers);
+            break;
+        case 1:
+            printf("Submarines: %u  |  Battleships: %u ", map[current_player].HUD.submarines,
+                    map[current_player].HUD.battleships);
+            break;
+        case 2:
+            printf("Carriers: %u    |", map[current_player].HUD.carriers);
+            break;
     }
 }
 
 void clear_screen(void)
 {
-    #ifdef __linux__ 
+#ifdef __linux__ 
     system("clear");
-    #elif defined WIN32
+#elif defined WIN32
     system("cls");
-    #endif
+#endif
+}
+
+char *fgets_(char *str, size_t size, FILE *stream)
+{
+    int ch;
+    unsigned i = 0;
+
+    while ((ch = getc(stream)) != '\n' && ch != EOF)
+        if (i < size)
+            str[i++] = ch;
+    if (i == 0 && ch == EOF)
+        return NULL;
+    str[i] = '\0';
+
+    return str;
 }
