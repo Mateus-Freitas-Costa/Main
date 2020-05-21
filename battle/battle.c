446175d4c43aefebc812644f825097ea658211e0
Mateuzz <38082919+Mateus-F@users.noreply.github.com>
2020-05-20 21:02:49 -0300

    Update battle.c

--- battle/battle.c
+++ battle/battle.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "battle.h"
 
 #define match_points(p1, p2) (p1.x == p2.x && p1.y == p2.y)
-
-static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], unsigned int size, Types type, const char *name);
 
 Boat *get_boat(Boat *boats, Point attack, int boats_count)
 {
@@ -16,54 +15,64 @@ Boat *get_boat(Boat *boats, Point attack, int boats_count)
     return NULL;
 }
 
-
-void create_map(Info info, Map *map, Boat boats[MAX_PLAYERS][info.boats])
-{
-    const Types pattern[5] = {CARRIER, SUBMARINE, DESTROYER,
-                                         BATTLESHIP, CRUISER};
-    const int pattern_size[5] = {5, 3, 2, 4, 3};
-
-    static const char *names[5] = {"Carrier", "Submarine", "Destroyer", "Battleship",
-                                    "Cruiser"};
-
-    for (int i = 0; i < MAX_PLAYERS; ++i) {
-        map[i].remaining_boats = 0;
-        map[i].HUD.battleships = 0;
-        map[i].HUD.cruisers = 0;
-        map[i].HUD.carriers = 0;
-        map[i].HUD.submarines = 0;
-        map[i].HUD.destroyers = 0;
-        map[i].info = info;
-        for (int j = 0; j < HEIGHT; ++j) 
-            for (int k = 0; k < WIDTH; ++k) 
-                map[i].sea[j][k] = INACTIVE;
-    }
-
-    for (int i = 0; i < info.boats; ++i) {
-        Types type = pattern[i % 5];
-        int size = pattern_size[type];
-        const char *name = names[type];
-
-        for (Player j = 0; j < MAX_PLAYERS; ++j) {
-            while (!create_boat(&boats[j][i], boats[j], map[j].sea, size, type, name))
-                ;
-            update_hud(&map[j].HUD, type, +1);
-            ++map[j].remaining_boats;
-        }
-    }
-
-}
-
 Validity check_valid(Camp sea[HEIGHT][WIDTH], Point attack)
 {
     if (attack.x < 0 || attack.y < 0
         || attack.x >= WIDTH || attack.y >= HEIGHT)
         return OUTRANGE;
-    else if (sea[attack.y][attack.x] == INACTIVE 
-             || sea[attack.y][attack.x] == ACTIVE)
-        return VALID;
-    else
+    else if (sea[attack.y][attack.x] == MISSED 
+            || sea[attack.y][attack.x] == DESTROYED 
+            || sea[attack.y][attack.x] == SUNK)
         return REPEATED;
+    else
+        return VALID;
+}
+
+void initialize_map(Map *map, Info info)
+{
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
+        map[i].remaining_boats = 0;
+        map[i].HUD = (Hud){0, 0, 0, 0, 0};
+        map[i].info = info;
+        for (int j = 0; j < HEIGHT; ++j)
+            for (int k = 0; k < WIDTH; ++k)
+                map[i].sea[j][k] = INACTIVE;
+    }
+}
+
+typedef struct BoatMetaInfo {
+    unsigned int size;
+    Types        type;
+    const char  *name;
+} BoatMetaInfo;
+
+static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], const BoatMetaInfo *boat);
+static BoatMetaInfo boat_create_meta_info(int i);
+
+static BoatMetaInfo boat_create_meta_info(int i)
+{
+    static const Types pattern[5] = {CARRIER, SUBMARINE, DESTROYER,
+                                         BATTLESHIP, CRUISER};
+    static const int pattern_size[5] = {5, 3, 2, 4, 3};
+    static const char *names[5] = {"Carrier", "Submarine", "Destroyer", "Battleship",
+                                    "Cruiser"};
+
+    int j = i % 5;
+    return (BoatMetaInfo){pattern_size[j], pattern[j], names[j]};
+}
+
+void create_map(Info info, Map *map, Boat boats[MAX_PLAYERS][info.boats])
+{
+    for (int i = 0; i < info.boats; ++i) {
+        BoatMetaInfo boat = boat_create_meta_info(i);
+
+        for (Player j = 0; j < MAX_PLAYERS; ++j) {
+            while (!create_boat(&boats[j][i], boats[j], map[j].sea, &boat))
+                ;
+            update_hud(&map[j].HUD, boat.type, +1);
+            ++map[j].remaining_boats;
+        }
+    }
 }
 
 void update_hud(Hud *HUD, Types type, int change)
@@ -88,11 +97,12 @@ void update_hud(Hud *HUD, Types type, int change)
 }
 
 
-static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], unsigned int size, Types type, const char *name)
+static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], const BoatMetaInfo *boat)
 {
-    new->p= malloc(size * sizeof(*new->p));
+    new->p= malloc(boat->size * sizeof(*new->p));
     if (new->p == NULL) {
-        free(new);
+        free(new->p);
+        fprintf(stderr, "Not enough memory\n");
         exit(EXIT_FAILURE);
     }
 
@@ -100,7 +110,7 @@ static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], unsigne
     int x = rand() % WIDTH;
     int y = rand() % HEIGHT;
     
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < boat->size; ++i) {
         if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0) {
             free(new->p);
             return false;
@@ -126,11 +136,11 @@ static bool create_boat(Boat *new, Boat *boats, Camp sea[HEIGHT][WIDTH], unsigne
             break;
         }
     }
-    new->lifes = size;
-    new->size  = size;
-    new->name  = name; 
-    new->type = type;
-    for (int i = 0; i < size; ++i) 
+    new->lifes = boat->size;
+    new->size  = boat->size;
+    new->name  = boat->name; 
+    new->type = boat->type;
+    for (int i = 0; i < boat->size; ++i) 
             sea[new->p[i].y]
                [new->p[i].x] = ACTIVE;
     
